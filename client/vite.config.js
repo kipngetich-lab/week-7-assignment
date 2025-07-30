3401493d6e42ab0047c0e5ad066d8131eee993a5
Kipngetich Josphat <joskipngetich07@gmail.com>
2025-07-30 11:59:10 +0300

    backend configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../client/dist',
+  },
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
